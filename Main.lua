cf161b5f52e3aa0f7e456c76ad4ccb8cd4acd2a8
The coolest farley <surelyilikemen@gmail.com>
2024-05-11 23:33:14 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,6 +3,6 @@ local id = game.PlaceId
 local Name = game:GetService("MarketplaceService"):GetProductInfo(id).Name
 
 if id == 662417684 then -- Lucky blocks battlegrounds
-    warn("Game is supported("..Name"). Loading script")
+    warn("Game is supported("..Name.."). Loading script")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Farls-Xavier/LuckyBlocks/main/Main.lua"))()
 end
