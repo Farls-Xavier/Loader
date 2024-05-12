5799fa51d08f92280894f35800d7eb5b0ed8d119
The coolest farley <surelyilikemen@gmail.com>
2024-05-11 22:25:12 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 local id = game.PlaceId
 
-if id == 662417684 then
-    warn("Game is supported(Lucky blocks). Loading script")
+if id == 662417684 then -- Lucky blocks battlegrounds
+    warn("Game is supported("..game.Name.."). Loading script")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Farls-Xavier/LuckyBlocks/main/Main.lua"))()
 end
