e3526431a1aada0fb126420b6bfb803331a7f81c
The coolest farley <surelyilikemen@gmail.com>
2024-05-11 22:15:11 -0500

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,6 @@
+local id = game.PlaceId
+
+if id == 662417684 then
+    -- LUCKY BLOCKS Battlegrounds
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Farls-Xavier/LuckyBlocks/main/Main.lua"))()
+end
